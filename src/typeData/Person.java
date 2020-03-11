b5e663ab11ed0598f9c9087cee0cef3338c9e36b
Langue <louis-clement.langue@isen.yncrea.fr>
2020-03-11 15:37:25 +0100

    Add person class

---
+++ src/typeData/Person.java
@@ -0,0 +1,78 @@
+package typeData;
+
+import java.util.Date;
+
+public class Person {
+    private int id;
+    private String lastname;
+    private String fisrtname;
+    private String nickname;
+    private String phoneNumber;
+    private String adress;
+    private String emailAdress;
+    private Date birthDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFisrtname() {
+        return fisrtname;
+    }
+
+    public void setFisrtname(String fisrtname) {
+        this.fisrtname = fisrtname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
