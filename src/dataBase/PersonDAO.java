b62eaae4b012bd572f67fa03b02b0a794ae7c28e
Langue <louis-clement.langue@isen.yncrea.fr>
2020-03-13 19:01:35 +0100

    add ride and sort database

--- src/dataBase/PersonDAO.java
+++ src/dataBase/PersonDAO.java
@@ -1,7 +1,38 @@
 package dataBase;
 
+import typeData.Person;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class PersonDAO {
-    public int three(){
-        return 3;
+
+    public ArrayList<Person> listPersonnByLastname(){
+        ArrayList<Person> byLastname = new ArrayList<Person>();
+        try (Connection connection = DataSourceFactory.getConnexion()){
+            try(Statement statement = connection.createStatement()){
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM person ORDER BY lastname")){
+                    while (resultSet.next()){
+                        Person person = new Person();
+                        person.setId(resultSet.getInt("idperson"));
+                        person.setLastname(resultSet.getString("lastname"));
+                        person.setFirstname(resultSet.getString("firstname"));
+                        person.setNickname(resultSet.getString("nickname"));
+                        person.setPhoneNumber(resultSet.getString("phone_number"));
+                        person.setAdress(resultSet.getString("address"));
+                        person.setEmailAddress(resultSet.getString("email_address"));
+                        person.setBirthDate(resultSet.getDate("birth_date"));
+                        byLastname.add(person);
+                    }
+                }
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Error reading", e);
+        }
+        return byLastname;
     }
+
 }
--- src/test/PersonDAOTest.java
+++ src/test/PersonDAOTest.java
@@ -1,17 +1,45 @@
 package test;
 
+import dataBase.DataSourceFactory;
 import dataBase.PersonDAO;
+import org.junit.Before;
 import org.junit.Test;
+import typeData.Person;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.Assert.*;
-
 public class PersonDAOTest {
 
+    @Before
+    public void initDataBase() throws SQLException {
+        Connection connection = DataSourceFactory.getConnexion();
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("DELETE from person");
+        statement.executeUpdate("INSERT INTO person(lastname, firstname, nickname, phone_number, address, email_address, birth_date) " +
+                "VALUES ('name12', 'name22', 'name32', '0123456789', 'isen2', 'address@isen.fr', '2020-05-02')");
+        statement.executeUpdate("INSERT INTO person(lastname, firstname, nickname, phone_number, address, email_address, birth_date) " +
+                "VALUES ('name11', 'name21', 'name31', '0123456789', 'isen1', 'address@isen.fr', '2020-05-02')");
+        statement.executeUpdate("INSERT INTO person(lastname, firstname, nickname, phone_number, address, email_address, birth_date) " +
+                "VALUES ('name13', 'name23', 'name33', '0123456789', 'isen3', 'address@isen.fr', '2020-05-02')");
+        statement.close();
+        connection.close();
+    }
+
     @Test
-    public void three() {
+    public void selectAllPersonnTest(){
         PersonDAO personDAO = new PersonDAO();
-        assertThat(personDAO.three()).isEqualTo(3);
+        ArrayList<Person> personList = personDAO.listPersonnByLastname();
+        assertThat(personList).hasSize(3).doesNotContainNull();
+        assertThat(personList.get(0).getLastname()).isEqualTo("name11");
+        assertThat(personList.get(0).getFirstname()).isEqualTo("name21");
+        assertThat(personList.get(0).getNickname()).isEqualTo("name31");
+        assertThat(personList.get(0).getPhoneNumber()).isEqualTo("0123456789");
+        assertThat(personList.get(0).getAdress()).isEqualTo("isen1");
+        assertThat(personList.get(0).getEmailAddress()).isEqualTo("address@isen.fr");
     }
 }
