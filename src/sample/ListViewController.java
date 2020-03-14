8e0d212ec3cc523e3476f9f065faaa866533b1aa
Ployt <florentin.dehooghe@isen.yncrea.fr>
2020-03-14 12:07:12 +0100

    ViewData on field

--- src/sample/ListViewController.java
+++ src/sample/ListViewController.java
@@ -1,15 +1,32 @@
 package sample;
 
+import dataBase.PersonDAO;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import service.StageService;
+import typeData.Person;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ListViewController {
+public class ListViewController implements Initializable {
     public void handleAdd(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("addelement.fxml"));
@@ -17,5 +34,38 @@ public class ListViewController {
         Scene scene = new Scene(rootAnchorPane);
         Stage ps = StageService.getInstance().getPrimaryStage();
         ps.setScene(scene);
+    }
+
+    public TableView<Person> tableview;
+    public TableColumn<Person, String> colfirstname;
+    public TableColumn<Person, String> collastname;
+    public Label firstnamefield;
+    public Label lastnamefield;
+    public Label nicknamefield;
+    public Label numberfield;
+    public Label adressefield;
+    public Label mailfield;
+    public Label birthdayfield;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colfirstname.setCellValueFactory(new PropertyValueFactory<Person, String>("firstname"));
+        collastname.setCellValueFactory(new PropertyValueFactory<Person, String>("lastname"));
+        tableview.setItems(observableList);
+
+    }
+
+    ObservableList<Person> observableList = FXCollections.observableList(
+            new PersonDAO().listPersonByLastname());
+
+    public void display(MouseEvent mouseEvent) {
+        Person person = tableview.getSelectionModel().getSelectedItem();
+        firstnamefield.setText(person.getFirstname());
+        lastnamefield.setText(person.getLastname());
+        nicknamefield.setText(person.getNickname());
+        numberfield.setText(person.getPhoneNumber());
+        adressefield.setText(person.getAdress());
+        mailfield.setText(person.getEmailAddress());
     }
 }
